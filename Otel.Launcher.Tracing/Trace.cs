872f86856642bc259c0a9cc21b9d62b7d6be8ee6
Sami Musallam <samusall@cisco.com>
2022-04-06 19:06:36 +0300

    Created basic otel launcher

---
+++ Otel.Launcher.Tracing/Trace.cs
@@ -0,0 +1,16 @@
+﻿namespace Otel.Launcher.Tracing;
+
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+public static class Trace
+{
+    public static void Init()
+    {
+        Sdk.CreateTracerProviderBuilder()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter()
+            .AddConsoleExporter()
+            .Build();
+    }
+}   
