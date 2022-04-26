78f1a84db7e9eb441e2975052c433e810229ef99
Sami Musallam <samusall@cisco.com>
2022-04-26 14:54:04 +0300

    Feat/Add Examples (#8)
    * feat(add-examples): added ASP NetCore example
    
    * Fix code style issues with dotnet_format
    
    * feat(add-examples): added console app example
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Cisco.Otel.Distribution.Tracing;
+
+var ciscoOptions =
+    new CiscoOptions(
+        "my-cisco-token",
+        "dotnet-console-example");
+
+var tracerProvider = Tracer.Init(ciscoOptions);
+
+Console.WriteLine("Hello, World!");
