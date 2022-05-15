a75069a2c36acd8eadcdf4daf5cd5a9de1091e5e
Sami Musallam <samusall@cisco.com>
2022-05-15 16:58:13 +0300

    fix(auto-versioning): script edits files in place (#24)

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "::set-output name=new_release_version::$1"
-sed "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" $2
-sed "s#<Version>.*#<Version>$1</Version>#" $2
+sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" $2
+sed -i "s#<Version>.*#<Version>$1</Version>#" $2
